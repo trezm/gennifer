a5ce39710940b8c9040e6255bc305f23febbcae2
Pete Mertz <pmertz@circle.com>
2018-01-22 11:21:22 -0500

    Add ssr templates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const ejs = require('ejs');
 const path = require('path');
-const fs = require('fs');
+const fs = require('node-fs-extra');
 
 start(process.argv);
 
@@ -41,6 +41,89 @@ function start(args) {
 
   if (type === 'component' || type === 'c') { compileComponent(name, options); }
   else if (type === 'service' || type === 's') { compileService(name, options); }
+  else if (type === 'noangular' || type === 'n') { compileNoAngularPage(name, options); }
+}
+
+/**
+ * @param {string} name
+ * @param {Options} options
+ */
+function compileNoAngularPage(name, options) {
+  if (options.prefix) {
+    name = options.prefix + name[0].toUpperCase() + name.slice(1);
+  }
+
+  const className = name[0].toUpperCase() + name.slice(1);
+  const instanceName = name[0].toLowerCase() + name.slice(1);
+  const snakeCaseName = instanceName.replace(/([A-Z])/g, (group1) => `-${group1.toLowerCase()}`);
+
+  if (!options.dryRun) {
+    fs.mkdirSync(`./src/views/${instanceName}`);
+    fs.mkdirSync(`./src/assets/${instanceName}`);
+    fs.copySync(`${__dirname}/assets/icons`, `./src/views/${instanceName}/icons`);
+  }
+
+  Promise.resolve()
+    // .then(() => compileTemplate('component.js', { className: className.replace(options.prefix, ''), instanceName: instanceName }))
+    // .then((compiledTemplate) => writeFileWrapper(path.resolve(`./src/assets/components/${instanceName}/${instanceName}.component.js`), compiledTemplate, options))
+    .then(() => compileTemplate('raw-page.ejs', { snakeCaseName: snakeCaseName, instanceName: instanceName }))
+    .then((compiledTemplate) => writeFileWrapper(path.resolve(`./src/views/${instanceName}/index.ejs`), compiledTemplate, options))
+    .then(() => compileTemplate('raw-header.ejs', { instanceName: instanceName }))
+    .then((compiledTemplate) => writeFileWrapper(path.resolve(`./src/views/${instanceName}/_${instanceName}Header.ejs`), compiledTemplate, options))
+    .then(() => compileTemplate('raw-footer.ejs', { instanceName: instanceName }))
+    .then((compiledTemplate) => writeFileWrapper(path.resolve(`./src/views/${instanceName}/_${instanceName}Footer.ejs`), compiledTemplate, options))
+    .then(() => compileTemplate('raw-page.css.less', { snakeCaseName: snakeCaseName }))
+    .then((compiledTemplate) => writeFileWrapper(path.resolve(`./src/assets/${instanceName}/${instanceName}-application.css.less`), compiledTemplate, options))
+    .then(() => compileTemplate('raw-footer.css.less', {}))
+    .then((compiledTemplate) => writeFileWrapper(path.resolve(`./src/assets/${instanceName}/footer.css.less`), compiledTemplate, options))
+    .then(() => {
+console.log(`
+Success! Note, I didn't add the following, which you'll want to do in order to see the page:
+
+1. Add a line to src/controllers/pages.js, something like:
+
+function ${instanceName}(req, res) {
+  return res.render('${instanceName}/index');
+}
+
+...
+
+module.exports = {
+  ...
+  ${instanceName},
+  ...
+};
+
+2. Add a route to src/routes/angular.js, something like:
+
+/**
+ * ${instanceName} page
+ */
+router.get('/some-magnificent-page', PagesController.${instanceName});
+
+3. Add a pipeline to the Brocfile, something likeL:
+
+/*
+ * css (less) for ${snakeCaseName}
+ */
+var ${instanceName}Css = less('src/assets/${instanceName}', '${instanceName}-application.css.less', '${instanceName}-application.css', {
+  paths: ['src/assets/${instanceName}'],
+  compress: config.compress
+});
+${instanceName}Css = autoprefixer(${instanceName}Css);
+${instanceName}Css = funnel(${instanceName}Css, {
+  destDir: 'stylesheets'
+});
+
+...
+
+var assets = mergeTrees([
+  ...
+  ${instanceName}Css
+  ...
+]);
+`);
+    });
 }
 
 /**
@@ -135,16 +218,16 @@ function writeFileWrapper(filePath, data, options){
  * @param {string} templateName
  * @param {any} data
  * @param {Options} [options]
- * @returns {Promise.<undefined>}
+ * @returns {Promise.<string>}
  */
 function compileTemplate(templateName, data, options) {
   return new Promise((resolve, reject) => {
-    ejs.renderFile(`${__dirname}/templates/${templateName}.ejs`, data, options, (err, str) => (err && reject(err)) || resolve(str));
+    ejs.renderFile(`${__dirname}/templates/${templateName}.ejs`, data, {}, (err, str) => (err && reject(err)) || resolve(str));
   });
 }
 
 /**
- * @returns {undefined}
+ * @returns {void}
  */
 function displayHelp() {
   console.log(`usage: gennifer <type> <name>
@@ -155,6 +238,7 @@ function displayHelp() {
   Types:
     component, c    Generate a new component
     service, s      Generate a new service
+    noangular, n    Generate a no-angular page
 `);
   process.exit();
 }
